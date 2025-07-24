0e2de8145a8c96dcc321e9d75d3251b6bea3fb58
josegrvm <josegrvm@gmail.com>
2025-07-24 12:49:53 -0400

    prebootcamp

--- 01Prebootcamp/Practica.js
+++ 01Prebootcamp/Practica.js
@@ -115,7 +115,7 @@ function evaluarNotas(){
 evaluarNotas();
 
 */
-
+/*
 // Bloque 1
 
 function saluda() {
@@ -392,3 +392,240 @@ if (names.length == 4) {
 } else {
     console.log('Error');
 }
+
+
+var x = 50;
+function test() {
+    var x = 10;
+}
+test();
+console.log(x);
+
+var x = 5;
+function updateValue() {
+    var x = 10;
+    console.log(x);
+}
+updateValue();
+console.log(x);
+
+
+var x = [10, 20, 30];
+function modifyArray(arr) {
+    arr[1] = 50;
+    console.log(arr);
+}
+modifyArray(x);
+console.log(x);
+
+
+
+var num = 100;
+function doubleValue(n) {
+    return n * 2;
+}
+var result = doubleValue(num);
+console.log(result);
+console.log(num);
+
+
+
+var count = 0;
+function increment() {
+    count++;
+}
+increment();
+increment();
+increment();
+
+console.log(count);
+
+
+
+var x = 3;
+function multiply(n) {
+    return n * n;
+}
+var result = multiply(x) + multiply(4);
+console.log(result);
+
+
+
+var values = [1, 2, 3, 4];
+function sumArray(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(sumArray(values));
+
+
+
+var x = [5, 10, 15];
+function modify(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] += 5;
+    }
+    return arr;
+}
+console.log(modify(x));
+
+*/
+
+function saluda() {
+
+    return "¡Hola mundo!";
+
+}
+
+var frase = saluda();
+
+console.log(); 
+
+
+var x = 10;
+function test() {
+    var x = 20;
+}
+test();
+console.log(x);
+
+
+var y = 5;
+if (y > 10) {
+    console.log('Greater');
+}
+
+var z = [10, 20, 30];
+z[1] = 50;
+console.log(z[1] == 20);
+
+
+var sum = 0;
+for (var i = 1; i <= 3; i++) {
+    sum += i;
+}
+console.log(sum == 7);
+
+
+function check(x) {
+    return x % 2 === 0;
+}
+console.log(check(3));
+
+
+var x = [1, 2, 3, 4];
+var y = x.length;
+console.log(y);
+
+
+var x = [10, 20, 30];
+function add(arr) {
+    arr.push(40);
+}
+add(x);
+console.log(x.length);
+
+
+var x = 10;
+function multiply(n) {
+    return n * 2;
+}
+var y = multiply(x);
+console.log(y);
+
+var x = 'Hello';
+var y = 'World';
+console.log(x + ' ' + y);
+
+var x = [1, 2, 3, 4];
+x[2] = 10;
+console.log(x[2]);
+
+
+var x = 5;
+function increment() {
+    x += 2;
+}
+increment();
+console.log(x);  // 7
+
+
+var arr = [5, 10, 15];
+function sumArray(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+console.log(sumArray(arr));  // 30
+
+
+var num = 4;
+var factorial = 1;
+for (var i = 1; i <= num; i++) {
+    factorial = factorial * i;
+}
+console.log(factorial);  // 24
+
+var message = 'Hello';
+var result = '';
+for (var i = 0; i < message.length; i++) {
+    result += message[i] + '-';
+}
+console.log(result);  //. H-e-l-l-o-
+
+
+var x = [2, 4, 6, 8];
+var result = [];
+for (var i = 0; i < x.length; i++) {
+    if (x[i] > 4) {
+        result.push(x[i]);
+    }
+}
+console.log(result); // [6, 8]
+
+
+var x = 1;
+while (x < 5) {
+    x++;
+}
+console.log(x); // 5
+
+
+var num = 10;
+function doubleValue(n) {
+    return n * 2;
+}
+console.log(doubleValue(num));  // 20
+console.log(num);  //. 10
+
+
+var names = ['Alice', 'Bob', 'Charlie'];
+names.push('David');
+console.log(names.length); // 4
+
+
+
+var x = [1, 2, 3, 4];
+function multiplyArray(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] *= 2;
+    }
+    return arr;
+}
+console.log(multiplyArray(x)); [2, 4, 6, 8]
+
+
+
+function saluda() {
+
+    return "¡Hola mundo!";
+
+}
+
+console.log(saluda()); //Imprime aquí ¡Hola mundo!
+
