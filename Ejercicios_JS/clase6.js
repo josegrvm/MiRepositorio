b60412b1b5a31c2f3a500ca6407c303c82cbb5c1
josegrvm <josegrvm@gmail.com>
2025-08-02 21:00:25 -0400

    listo

---
+++ Ejercicios_JS/clase6.js
@@ -0,0 +1,12 @@
+/*let edad = 12;
+let dinero = 600;
+let esVip = false;
+
+
+if(edad > 13 && dinero > 300 || esVip === true){
+
+    console.log("Puedes ingresar");
+}else {
+    console.log("Lo siento, no puedes ingresar")
+}*/
+
