4703ef61bd7d17a7e78020f1642696ddc54b2f00
josegrvm <josegrvm@gmail.com>
2025-08-22 17:50:59 -0400

    nueva tarea

---
+++ MERN/JS-Moderno/Tareas/flechasCondicionales.js
@@ -0,0 +1,38 @@
+
+//jercicio 1: Conversor de temperatura
+
+//Crea una función flecha que convierta temperaturas de grados Celsius a Fahrenheit. La fórmula para convertir Celsius
+//a Fahrenheit es: Fahreneit = (Celsius * 9 / 5) + 32
+
+const CerlsiusAFahrenheit = (cerlsius) => fahrenheit = (cerlsius * 9/5) + 32;
+
+
+
+
+// Ejercicio 2: Generador de mensajes personalizados
+
+// Implementa una función flecha que tome un nombre y una edad como argumentos y devuelva un mensaje en formato de cadena, por ejemplo:
+
+// "Hola Juan, tienes 30 años de edad."
+
+
+
+
+
+
+//  Ejercicio 3: Convertir de millas a kilómetros
+
+// Crea una función flecha que convierta millas a kilómetros. Considera que: 1 milla = 1.60934 km
+
+
+
+
+
+
+//  Ejercicio 4: Consejos según el clima
+
+// Crea una función flecha que, según el clima ingresado como argumento, retorne un consejo adecuado. Por ejemplo:
+
+// Si el clima es “lluvioso”, debe sugerir llevar un paraguas.
+// Si el clima es “soleado”, debe sugerir llevar un sombrero.
+// Utiliza el operador ternario para simplificar la evaluación condicional en este ejercicio.
