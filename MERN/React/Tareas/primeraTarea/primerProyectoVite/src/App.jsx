cca110e1b0c9cc0fd8839e3c4de4f89da7229528
josegrvm <josegrvm@gmail.com>
2025-09-03 01:31:41 -0400

    primerCore listo

---
+++ MERN/React/Tareas/primeraTarea/primerProyectoVite/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body>
+        <h1>¡Bienvenido a mi aplicación de React!</h1>
+        <h2>Lista de cosas por hacer</h2>
+          <li>Aprender React</li>
+          <li>Practicar con Vite</li>
+          <li>Construir proyectos increíbles</li>
+      </body>
+    </>
+  )
+}
+
+export default App
