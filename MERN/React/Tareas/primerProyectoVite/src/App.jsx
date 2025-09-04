0457aec0566255b424263b731a4a557ee12581b8
josegrvm <josegrvm@gmail.com>
2025-09-04 00:58:06 -0400

    Ambas tareas lista

---
+++ MERN/React/Tareas/primerProyectoVite/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Lista from './Components/Lista'
+
+function App() {
+
+  return (
+    <>
+      <body>
+        <h1>¡Bienvenido a mi aplicación de React!</h1>
+        <Lista />
+      </body>
+    </>
+  );
+};
+
+export default App
---
+++ MERN/React/Tareas/primerProyectoVite/src/Components/Lista.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Lista() {
+    return (
+    <div className="listados">
+        <h2>Lista de cosas por hacer</h2>
+        <ul>
+            <li>Aprender React</li>
+            <li>Practicar con Vite</li>
+            <li>Construir proyectos increíbles</li>
+        </ul>
+    </div>
+    );
+};
+
+export default Lista;
